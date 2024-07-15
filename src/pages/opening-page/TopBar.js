cc9aeac5c0275bd36d3d01258e7e3cfa546721e6
muhammedyaydogan <muhammed.aydogan@aol.com>
2024-07-15 17:59:22 +0300

    topbar improvement

--- src/pages/opening-page/TopBar.js
+++ src/pages/opening-page/TopBar.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { CollersButton } from "../../components/CollersButton";
+import CollersIcon from "../../components/CollersIcon";
 
 export const TopBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = (e) => {
+    e.stopPropagation();
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  const createButtons = () => {
+    const buttonProps = [
+      { text: "Products" },
+      { text: "Solutions" },
+      { text: "Pricing" },
+      { text: "Resources" },
+      { text: "Log In" },
+      { text: "Sign Up Now", border: true },
+    ];
+
+    return buttonProps.map((props, i) => <CollersButton key={i} {...props} />);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="bg-transparent text-primary w-full py-6 px-20 flex justify-between items-center">
+    <div className="flex flex-row py-6 px-20 justify-between items-center flex-wrap">
       <h1 className="text-[32px] font-sans font-bold">COLLERS</h1>
-      <div className="flex flex-row">
-        <CollersButton text="Products" />
-        <CollersButton text="Solutions" />
-        <CollersButton text="Pricing" />
-        <CollersButton text="Resources" />
-        <CollersButton text="Lon In" />
-        <CollersButton text="Sign Up Now" border={true} />
+      <div className="bg-transparent text-primary flex items-center">
+        <div className="hidden md:flex md:flex-row space-x-4">
+          {createButtons()}
+        </div>
+        <div className="block md:hidden">
+          <CollersButton
+            svg={<CollersIcon name="menu" />}
+            onClick={toggleMenu}
+          />
+        </div>
       </div>
+
+      {isMenuOpen && (
+        <div
+          ref={menuRef}
+          className={`absolute top-16 right-20 bg-light-orange bg-opacity-90 shadow-lg rounded-md z-10 md:hidden`}
+        >
+          <div className="flex flex-col p-4">{createButtons()}</div>
+        </div>
+      )}
     </div>
   );
 };
